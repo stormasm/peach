660ec20f5182cb93035d1a804697af67269f2a74
Michael Angerman <stormasm@gmail.com>
2015-02-06 09:10:57 -0500

    initial commit

---
+++ process/activity_events.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"code.google.com/p/goauth2/oauth"
+	"fmt"
+	"github.com/google/go-github/github"
+	"os"
+)
+
+var (
+	client *github.Client
+
+	// auth indicates whether tests are being run with an OAuth token.
+	// Tests can use this flag to skip certain tests when run without auth.
+	auth bool
+)
+
+func init() {
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		print("!!! No OAuth token.  Some tests won't run. !!!\n\n")
+		client = github.NewClient(nil)
+	} else {
+		t := &oauth.Transport{
+			Token: &oauth.Token{AccessToken: token},
+		}
+		client = github.NewClient(t.Client())
+		auth = true
+	}
+}
+
+func main() {
+
+	opt := &github.ListOptions{Page: 2, PerPage: 100}
+	events, _, err := client.Activity.ListEvents(opt)
+	if err != nil {
+		fmt.Println("Activities.ListEvents returned error: %v", err)
+	}
+
+	fmt.Printf("Number of events = %v\n", len(events))
+
+	for i, v := range events {
+		fmt.Printf("array value at [%d]=%s\n", i, *v.Type)
+	}
+
+	rate, _, err := client.RateLimit()
+	if err != nil {
+		fmt.Printf("Error fetching rate limit: %#v\n\n", err)
+	} else {
+		fmt.Printf("API Rate Limit: %d\n", rate.Remaining)
+	}
+
+}
