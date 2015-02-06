6c097014c5d577085f3b5ea3e5da710f3fe7b5dc
Michael Angerman <stormasm@gmail.com>
2015-02-06 11:11:56 -0500

    starting to look at the payload

--- parse/activity_events.go
+++ parse/activity_events.go
@@ -8,9 +8,19 @@ import (
 func Listevents(events []github.Event) {
 	fmt.Printf("Number of events = %v\n", len(events))
 
-	for i, v := range events {
-		fmt.Printf("[%d] %s  ", i, *v.Type)
-		fmt.Printf("%s", *v.Repo.Name)
+	for i, event := range events {
 		fmt.Printf("\n")
+		fmt.Printf("[%d] %s  ", i, *event.Type)
+		fmt.Printf("%s", *event.Repo.Name)
+		fmt.Printf("\n")
+		payload(event)
 	}
+}
+
+func payload(event github.Event) {
+	fmt.Println("\n")
+	//mystr := event.String()
+	//fmt.Println(mystr)
+	myrep := event.Payload()
+	fmt.Println(myrep)
 }
